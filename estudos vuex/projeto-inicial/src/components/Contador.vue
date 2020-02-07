e2ab289af5c676435e86bcc95b5caf14fa766b6c
William Chenta <william.chenta@gmail.com>
2020-02-07 15:51:05 -0300

    terminando o desenvolvimento

---
+++ estudos vuex/projeto-inicial/src/components/Contador.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+      {{count}}
+      <button @click="$store.commit('add')">Incrementar</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Contador",
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
